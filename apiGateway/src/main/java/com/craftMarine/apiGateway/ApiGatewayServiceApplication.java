81402c978e2c97460df0288df82a58f27905085b
Pradipto Daw <pradipto.daw1999@gmail.com>
2024-07-14 23:37:43 +0530

    Uploaded apigateway folder

---
+++ apiGateway/src/main/java/com/craftMarine/apiGateway/ApiGatewayServiceApplication.java
@@ -0,0 +1,16 @@
+package com.craftMarine.apiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServiceApplication.class, args);
+	}
+
+}
+ 
